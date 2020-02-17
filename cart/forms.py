80e31a4833eddcb250fbfc74223c00891667c7ff
Veo20 <vedantdbapodra@gmail.com>
2020-02-17 13:46:21 +0530

    Form Update

--- cart/forms.py
+++ cart/forms.py
@@ -3,18 +3,31 @@ from django import forms
 class ShippingForm(forms.Form):
 
     country_choice = [
+        ('', ''),
         ('india','India'),
-        ('china','China'),
+        ('usa','USA'),
+    ]
+
+    state_choice = [
+        ('', ''),
+        ('maharashtra', 'Maharashtra'),
+        ('gujurat', 'Gujurat'),
+        ('tamil nadu', 'Tamil Nadu'),
+        ('rajasthan', 'Rajasthan')
     ]
 
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
+    address = forms.CharField(widget=forms.Textarea(attrs={"rows":5, "cols":25}))
     phone = forms.IntegerField()
     country = forms.CharField(widget= forms.Select(choices=country_choice))
-    state = forms.CharField()
+    state = forms.CharField(widget= forms.Select(choices=state_choice))
     city = forms.CharField()
     zip_code = forms.CharField()
+    card_choices = [('1', 'Credit Card'), ('2', 'Debit Card')]
+    choice_field = forms.ChoiceField(label="Payment Choice",choices=card_choices, widget=forms.RadioSelect)
+
     
 
     
