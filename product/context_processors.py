495ba4445dee24b49834e62750fdb3d062baee10
Akhil-Shah <akhilkshah@gmail.com>
2020-03-31 18:18:12 +0530

    Fixed Bug

--- product/context_processors.py
+++ product/context_processors.py
@@ -4,7 +4,10 @@ def add_to_context(request):
 
     game_names = Game.objects.all()
 
-    no_of_items = len(request.session['cart_details'])
+    try:
+        no_of_items = len(request.session['cart_details'])
+    except:
+        no_of_items = 0
 
     context = {
         'game_names':game_names,
